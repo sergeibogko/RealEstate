17b0d8771828e3a611c350d23e3ed490f3ad0e31
Sergei Bogko <69677370+sergeibogko@users.noreply.github.com>
2022-12-07 19:28:21 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -9,3 +9,27 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.body-main-block__arrow.swiper-button-prev',
   },
 });
+
+// Табы
+
+const tabNavItems = document.querySelectorAll('.tabs-deals__button');
+const tabItems = document.querySelectorAll('.item-tabs');
+document.addEventListener('click', function (e) {
+  const targetElement = e.target;
+  let currentActiveIndex = null;
+  let newActiveIndex = null;
+  if (targetElement.closest('.tabs-deals__button')) {
+    tabNavItems.forEach((tabNavItems, index) => {
+      if (tabNavItems.classList.contains('active')) {
+        currentActiveIndex = index;
+        tabNavItems.classList.remove('active');
+      }
+      if (tabNavItems === targetElement) {
+        newActiveIndex = index;
+      }
+    });
+    targetElement.classList.add('active');
+    tabItems[currentActiveIndex].classList.remove('active');
+    tabItems[newActiveIndex].classList.add('active');
+  }
+});
